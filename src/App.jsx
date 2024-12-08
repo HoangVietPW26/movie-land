d70410ed979c2f6c39983fa8145c84af84b1951e
HoangVietPW26 <nhviet26@gmail.com>
2024-12-08 16:19:45 +0000

    fix env

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,10 @@ import "./App.css";
 import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
 
-// 3524edc7
-const API_URL = "http://www.omdbapi.com/?apikey=3524edc7";
-const movie1 = {
-  Title: "Italian Spiderman",
-  Year: "2007",
-  imdbID: "tt2705436",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BYWNiMmNlNmQtZTI2MS00MzAxLTgxM2QtNDY3ZGQxNDMwZDgzXkEyXkFqcGc@._V1_SX300.jpg",
-};
+const API_URL = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
 
 const App = () => {
+  console.log(API_URL);
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
